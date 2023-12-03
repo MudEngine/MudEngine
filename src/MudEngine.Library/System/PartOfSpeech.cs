c286c552c8cfca6fcc196c007ab44a9c2a69e7fb
Mud Engine <mudengine@slayers.co.uk>
2023-12-03 15:34:06 +0000

    Replace OpenNLP with basic POS tagging

--- src/MudEngine.Library/System/PartOfSpeech.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MudEngine.Library.System;
-
-public class PartOfSpeech(string name, int index, string type)
-{
-    public string? Token { get; set; } = name;
-    public int Index { get; set; } = index;
-    public string Type { get; set; } = type;
-}
